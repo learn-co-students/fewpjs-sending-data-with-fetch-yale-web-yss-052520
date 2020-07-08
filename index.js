dd36654d95f4c33f0b2b530cef6a6e8552059b0a
edensongjbs <edensongjbs@gmail.com>
2020-07-08 17:16:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Add your code here
+function formData(name, email) {
+    return {
+    name: name ,
+    email: email
+    }
+}
+   
+function configObj(name, email) {
+    return {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData(name, email))
+    }
+}
+ 
+function submitData(name, email) {
+    const theObj = configObj(name, email)
+    debugger
+    return fetch("http://localhost:3000/users", theObj)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+        const h1 = document.createElement('h1')
+        document.body.innerText = object.id
+        // document.body.append(h1)
+        debugger
+        // console.log(object);
+    })
+    .catch(function(error) {
+    //   alert("Bad things! Ragnarők!");
+    //   console.log(error.message);
+        debugger
+        const h2 = document.createElement('h2')
+        document.body.innerText = error.message
+        // document.body.append(h2)
+        debugger
+        // console.log(object);
+    })
+}
