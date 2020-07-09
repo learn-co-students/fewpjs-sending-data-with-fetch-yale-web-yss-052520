b3de20f9689a45bb42067767c5b0091ae5b01da3
Drew Beckmen <drew.beckmen@yale.edu>
2020-07-08 21:05:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "content-Type": "application/json", 
+            "accept": "application/json"
+        }, 
+        body: JSON.stringify({
+            name, 
+            email 
+        })
+    })
+    .then (response => response.json())
+    .then (function(obj) {
+        document.body.innerHTML += obj["id"]
+    })
+    .catch(function(error) {
+        document.body.innerHTML += error
+    })
+}
