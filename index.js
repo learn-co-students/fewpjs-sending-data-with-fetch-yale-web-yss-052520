8d5a27861dfb9b0f4f05dca070990aa48c88acc7
justin_james <jjames11799@gmail.com>
2020-07-13 13:12:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData(name, email) {
+    const url = `http://localhost:3000/users`
+
+   
+
+    let configObject = {
+        method: "POST",
+        headers: {
+            'Content-type':'application/json',
+            'Accept':'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            email 
+        })
+    }
+    return fetch(url, configObject)
+    .then(response => response.json())
+    .then(data => {
+        // let newName = document.createElement('p')
+        // newName.innerText = data.name
+        // let newEmail = document.createElement('p')
+        // newEmail.innerText = data.email
+        return document.body.innerHTML = data.id })
+        .catch(error => {
+            let messages = document.createElement('p')
+            messages.innerHTML = error.message
+            return document.querySelector('body').append(messages)
+        })
+}
