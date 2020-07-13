565ab5dacf4fc308cfd879cfbff38f91e106ec82
chad <chad.palmer@yale.edu>
2020-07-13 13:22:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+function submitData(name, email) {
+    let formData = {
+        name: name,
+        email: email
+    }
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+
+    return fetch('http://localhost:3000/users', configObj)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(object) {
+            document.body.append(object.id)
+        })
+        .catch(function(error) {
+            alert("AAAAAHHHH")
+            document.body.append(error.message)
+        })
+}
