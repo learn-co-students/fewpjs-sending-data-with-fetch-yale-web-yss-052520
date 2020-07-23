1a71a4a1fff78ebe615edef1d752535096a1d359
larissanguyen <larissa.h.nguyen@gmail.com>
2020-07-22 23:23:20 -0400

    test 1 passing

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your code here
+function submitData(name, email) {
+	let configObj = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+			},
+		body: JSON.stringify({name, email})
+	}
+
+	return fetch('http://localhost:3000/users', configObj)
+		.then(resp => resp.json())
+		.then(resp => document.querySelector('body').innerText += resp.id)
+}
