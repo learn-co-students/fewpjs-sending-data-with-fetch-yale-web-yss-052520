f733a8166e5f0472a06edad2ea305df05e0b704d
tbartelsmeyer <47281160+tbartelsmeyer@users.noreply.github.com>
2020-07-24 13:39:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add your code here
+function submitData( name, email ) {
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify( {name, email} )
+  }
+  return fetch('http://localhost:3000/users', configObj)
+    .then(response => response.json())
+      .then(object => document.body.innerHTML = object["id"])
+        .catch(error => document.body.innerHTML = error.message)
+}
